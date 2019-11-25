900b8e45e916b30997bf155c2e7dbf49fd8dbdf9
AndriusJau <57878844+AndriusJau@users.noreply.github.com>
2019-11-25 19:45:04 +0200

    Pradinė struktūra

---
+++ index.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Projekto pavadinimas</title>
+	
+	<?php include "head.php"; ?>
+
+</head>
+<body>
+
+	<?php include "header.php"; ?>
+	
+	<p>
+		Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+		tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+		quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+		consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
+		cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
+		proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+
+	</p>
+
+
+	<?php include "footer.php"; ?>
+	
+</body>
+</html>

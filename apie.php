a7f73f1e9e0d7ff5a9893ce48450d3b4accd314b
LigitaPB <58165645+LigitaPB@users.noreply.github.com>
2019-11-26 20:16:56 +0200

    Apie rubrika

---
+++ apie.php
@@ -0,0 +1,67 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Apie</title>
+	
+	<?php include "head.php"; ?>
+
+</head>
+<body>
+
+	<?php include "header.php"; ?>
+
+	<h1 class="center-align">More About P.Programuotojai Travel</h1>
+
+<div class="container">
+
+	<div class="pagrindinenuotrauka"> 
+		<img class="responsive-img" src="images/Pagrindinefoto.jpg">
+		<h2>Mūsų komanda</h2>
+	</div>
+	
+	<p>
+		P.Programuotojai Travel - kelionių agentūra, skirta išimtinai programuotojų kelionių organizavimui. Mes žinome, kad Jūsų darbui reikalingas:
+		kompiuteris;
+		greitas internetas;
+		geras oras ir baseinas, jūra arba ežeras kalnuose,
+
+		todėl siūlome pasirinkti Jūsų poreikius atitinkantį kelionės paketą.
+	</p>
+
+	  	<div class="row">
+	  	
+		  	<div class="col s12 m6 l3">
+		  		<img class="responsive-img" src="images/AndriusJ.jpg">
+		  		<p class="center-align">Andrius Jauniškis</p>
+		  		<p>Andrius J. Jums padės, jei norite rasti vietų sužaisti pokerio partiją.</p>
+		  	</div>
+
+		  	<div class="col s12 m6 l3">
+		  		<img class="responsive-img circle" src="images/AndriusN.jpg">
+		  		<p class="center-align">Andrius Neironis</p>
+		  		<p>Andrius N. Jums padės, jei esate didelis ekskursijų fanas.</p>
+		  	</div>
+
+		  	<div class="col s12 m6 l3">
+		  		<img class="responsive-img circle" src="images/Jurgita.jpg">
+		  		<p class="center-align">Jurgita Gerdvilė</p>
+		  		<p>Jurgita jei norite sutaupyti laiko kelionei - Jurgita Jums padės rasti poilsį Trakų rajone. </p>
+		  	</div>
+
+		  	<div class="col s12 m6 l3">
+		  		<img class="responsive-img circle" src="images/Ligita.jpg">
+		  		<p class="center-align">Ligita Poškaitė-Balkauskė</p>
+		  		<p>Ligita Jums padės, jei į poilsio paketą norite įtraukti ir apsilankymą tos šalies valstybės institucijose.</p>
+		  	</div>
+
+		</div>
+	
+</div>
+
+<iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2304.9836000007226!2d25.290128215814175!3d54.709911680287405!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x46dd96ae1dedd637%3A0x73978617430c730c!2sUlon%C5%B3%20g.%205%2C%20Vilnius%2008240!5e0!3m2!1slt!2slt!4v1574789193239!5m2!1slt!2slt" width="600" height="450" frameborder="0" style="border:0;" allowfullscreen=""></iframe>
+
+
+<?php include "footer.php"; ?>
+
+</body>
+</html>

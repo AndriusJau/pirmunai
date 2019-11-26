121f4efb9ed1e1839fa5d1fe53991e781572455f
LigitaPB <58165645+LigitaPB@users.noreply.github.com>
2019-11-26 21:17:52 +0200

    Apie rubrika

--- apie.php
+++ apie.php
@@ -21,10 +21,11 @@
 	
 	<p>
 		P.Programuotojai Travel - kelionių agentūra, skirta išimtinai programuotojų kelionių organizavimui. Mes žinome, kad Jūsų darbui reikalingas:
-		kompiuteris;
-		greitas internetas;
-		geras oras ir baseinas, jūra arba ežeras kalnuose,
-
+		<ol>
+			<li>kompiuteris;</li>
+			<li>greitas internetas;</li>
+			<li>geras oras ir baseinas, jūra arba ežeras kalnuose,</li>
+		</ol>
 		todėl siūlome pasirinkti Jūsų poreikius atitinkantį kelionės paketą.
 	</p>
 
@@ -58,8 +59,32 @@
 	
 </div>
 
+<img class="fiksuotas right" src="https://images/Slapukai.jpg">
+<img class="fiksuotas right" src="https://picsum.photos/200/300">
+
 <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2304.9836000007226!2d25.290128215814175!3d54.709911680287405!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x46dd96ae1dedd637%3A0x73978617430c730c!2sUlon%C5%B3%20g.%205%2C%20Vilnius%2008240!5e0!3m2!1slt!2slt!4v1574789193239!5m2!1slt!2slt" width="600" height="450" frameborder="0" style="border:0;" allowfullscreen=""></iframe>
 
+
+
+	<form action="https://google.com/search">
+		<input type="text" name="query">
+		<button>Ieškoti</button>
+	</form>
+
+		<a target="_blank" href="https://google.com">Nuoroda i</a>
+
+
+	<a href="https://www.w3schools.com">Visit W3Schools.com!</a>
+	
+
+<a target="_blank" href="https://google.com">
+	<button>Mygtukas</button>
+</a>
+
+
+	<button id="btn">Pakeisti spalva</button>
+	<button id="btn2">Pakeisti spalva (jQuery)</button>
+	
 
 <?php include "footer.php"; ?>
 

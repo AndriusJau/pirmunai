6abac4cc66448a3fd52b6666105e4a089de499ec
Jurgita <j.gerdvile@gmail.com>
2019-11-27 21:44:53 +0200

    PirmiJurgitos pakeitimai

--- kontaktai.php
+++ kontaktai.php
@@ -7,31 +7,102 @@
 
 </head>
 <body>
-<?php include "header.php"; ?>
+	<?php include "header.php"; ?>
 
-<h1>Kontaktai</h1>
-<p>
-P.Programuotojai Travel administruoja: <br>
+	<h1>Kontaktai</h1>
+	<div class="container">
+		<p>
+			P.Programuotojai Travel administruoja: <br>
 
-<br>UAB "P.Programuotojai Travel"<br>
-Įmonės kodas 302583106 <br>
-PVM mokėtojo kodas LT100005943011 <br>
-Adresas: Kęstučio g. 4, LT-21104 <br>
-Trakai, Lietuva <br>
+			<br>UAB "P.Programuotojai Travel"<br>
+			Įmonės kodas 302583106 <br>
+			PVM mokėtojo kodas LT100005943011 <br>
+			Adresas: Kęstučio g. 4, LT-21104 <br>
+			Trakai, Lietuva <br>
 
-Darbo laikas: <br>
-Telefonas: (8-528) 53945 <br>
-</p>
+			Darbo laikas: <br>
+			I-V 08:00-19:00 <br>
+			VI–VII 09:00-17:00 (tik skambučiai) <br>
+			Telefonas:<a href="tel:%2B370%20639%2096%20444">+370 655 55 555</a><br>
+			El. paštas: <a href="mailto:info@ptravel.lt">info@ptravel.lt</a><br>
+		</p>
+
+		<p>
+			Sekite mus socialiniuose tinkluose:
+
+		</p>
+
+	</p>
+
+	<div class="row">
+
+		<div class="col s12 m6 l3">
+			<img class="responsive-img circle" src="images/AndriusJ1.jpg">
+			<p class="center-align">Andrius Jauniškis</p>
+			<p>Telefonas: (8-611) 11 111 <br>
+				El. paštas: Jauniskis@ptravel.lt <br>
+			</p>
+			<a class="waves-effect waves-light btn"><i class="material-icons left">cloud</i>Contact</a>
+		</div>
+
+		<div class="col s12 m6 l3">
+			<img class="responsive-img circle" src="images/AndriusN1.jpg">
+			<p class="center-align">Andrius Neironis</p>
+			<p>Telefonas: (8-622) 22 222 <br>
+				El. paštas: Neironis@ptravel.lt <br>
+			</p>
+			<a class="waves-effect waves-light btn"><i class="material-icons left">cloud</i>Contact</a>
+		</div>
+
+		<div class="col s12 m6 l3">
+				<img class="responsive-img circle" src="images/Jurgita.jpg">
+				<p class="center-align">Jurgita Gerdvilė</p>
+				<p>Telefonas: (8-633) 33 333 <br>
+					El. paštas: Gerdvile@ptravel.lt <br></p>
+					<a class="waves-effect waves-light btn"><i class="material-icons left">cloud</i>Contact</a>
+		</div>
+
+		<div class="col s12 m6 l3">
+					<img class="responsive-img circle" src="images/Ligita.jpg">
+					<p class="center-align">Ligita Poškaitė-Balkauskė</p>
+					<p>Telefonas: (8-644) 44 444 <br>
+						El. paštas: Poskaite@ptravel.lt <br></p>
+						<a class="waves-effect waves-light btn"><i class="material-icons left">cloud</i>Contact</a>
+		</div>
+
+	</div>
 
 
 
+<div class="row">
 
-
-<div>
-	<iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2308.2563260413413!2d24.931442815857586!3d54.65231238027346!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x46ddf36c24788be3%3A0xc42354dd68ba6032!2sTrak%C5%B3%20salos%20pilis!5e0!3m2!1slt!2slt!4v1574875506772!5m2!1slt!2slt" width="600" height="450" frameborder="0" style="border:0;" allowfullscreen=""></iframe>
-</div>
-
-	<?php include "footer.php"; ?>
+				<div class="col s12 m6 l6">
+					<iframe class="zemelapis" 
+					src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2308.2563260413413!2d24.931442815857586!3d54.65231238027346!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x46ddf36c24788be3%3A0xc42354dd68ba6032!2sTrak%C5%B3%20salos%20pilis!5e0!3m2!1slt!2slt!4v1574875506772!5m2!1slt!2slt" height="450" frameborder="0" style="border:0;" allowfullscreen=""></iframe>
+				</div>
+<div class="zinute">
 	
-</body>
-</html>
+
+<div class="col s12 m6 l6">
+				<form action="kur siuncia duomenis ">
+					Vardas:<br>
+					<input type="text" name="Vardas" >
+					<br>
+					Pavarde:<br>
+					<input type="text" name="Pavarde" >
+					<br>
+					Jusu klausimas:<br>
+					<textarea class="materialize-textarea" name="klausymo_tekstas">
+					</textarea>
+					<br>
+					<br>
+					<input type="submit" value="Siusti">
+				</form> 
+</div>
+</div>
+	</div>
+</div>
+			<?php include "footer.php"; ?>
+
+		</body>
+		</html>

906e13c5f96439c931924eaaff5b718201027c09
AndriusJau <57878844+AndriusJau@users.noreply.github.com>
2019-11-28 20:27:46 +0200

    Andriaus titulinis

--- header.php
+++ header.php
@@ -1,19 +1,30 @@
  <nav>
     <div class="nav-wrapper container">
-      <a href="#!" class="brand-logo">P.Programuotojai Travel</a>
+      <a href="index.php" class="brand-logo">P.Programuotojai Travel</a>
       <a href="#" data-target="mobile-demo" class="sidenav-trigger"><i class="material-icons">menu</i></a>
       <ul class="right hide-on-med-and-down">
-        <li><a href="sass.html">Apie</a></li>
-        <li><a href="badges.html">Visi pasiūlymai</a></li>
-        <li><a href="collapsible.html">Kontaktai</a></li>
-        <li><a href="mobile.html">Mobile</a></li>
+        <li><a href="apie.php">Apie</a></li>
+        <li><a href="pasiulymai.php">Visi pasiūlymai</a></li>
+        <li><a href="kontaktai.php">Kontaktai</a></li>
+        <!-- <li><a href="mobile.html">Mobile</a></li> -->
       </ul>
     </div>
   </nav>
 
+  <nav>
+    <div class="nav-wrapper">
+      <div class="col s12">
+        <a href="index.php" class="breadcrumb">Pagrindinis</a>
+        <a href="apie.php" class="breadcrumb">Apie</a>
+        <a href="kontaktai.php" class="breadcrumb">Kontaktai</a>
+        <a href="pasiulymai.php" class="breadcrumb">Visi pasiūlymai</a>
+      </div>
+    </div>
+  </nav>
+
   <ul class="sidenav" id="mobile-demo">
-    <li><a href="sass.html">Apie</a></li>
-    <li><a href="badges.html">Visi pasiūlymai</a></li>
-    <li><a href="collapsible.html">Kontaktai</a></li>
-    <li><a href="mobile.html">Mobile</a></li>
+    <li><a href="apie.php">Apie</a></li>
+    <li><a href="pasiulymai.php">Visi pasiūlymai</a></li>
+    <li><a href="kontaktai.php">Kontaktai</a></li>
+    <!-- <li><a href="mobile.html">Mobile</a></li> -->
   </ul>
